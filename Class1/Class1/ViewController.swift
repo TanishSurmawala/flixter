9e18c1753d5fa8fb232d1de5557a29c2ad0ac48a
Tanish Surmawala <tanishsurmawala@gmail.com>
2020-10-03 03:14:10 -0500

    WEEK 2 FLIXTER

---
+++ Class1/Class1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class1
+//
+//  Created by Tanish Surmawala on 9/19/20.
+//  Copyright © 2020 Tanish Surmawala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ flixster/flixster/ovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  ovieGridCell.swift
+//  flixster
+//
+//  Created by Tanish Surmawala on 10/3/20.
+//  Copyright © 2020 Tanish Surmawala. All rights reserved.
+//
+
+import UIKit
+
+class ovieGridCell: UICollectionViewCell {
+    
+}
